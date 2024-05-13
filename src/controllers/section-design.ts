01857891fc8c1d1bd6f6635b783e7d7fa4b1d2f6
mohametalmeari <mohamet.almeari@gmail.com>
2024-05-14 01:39:03 +0300

    Refactor square section design API to handle compression reinforcement case

--- src/controllers/section-design.ts
+++ src/controllers/section-design.ts
@@ -12,10 +12,11 @@ export const square = async (req: Request, res: Response) => {
       moment: m,
       width: b,
       height: h,
-      concrete_cover: a,
+      concrete_cover,
       concrete_compressive_strength: fc,
       steel_yield_strength: fy,
       reinforcement_bars_diameter: d,
+      draft = false,
     } = req.body;
 
     // Check if required fields are present
@@ -23,20 +24,28 @@ export const square = async (req: Request, res: Response) => {
       return res.sendStatus(400);
     }
 
-    const depth = h - (a || 0.1 * h);
+    const a = concrete_cover || 0.1 * h;
+
+    const depth = h - a;
 
     const areaCo = m / (0.9 * 0.85 * fc * b * depth ** 2);
 
     const alphaCo = 1 - (1 - 2 * areaCo) ** 0.5;
     const alphaMaxCo = 267.75 / (630 + fy);
 
+    let gamaCo = 1 - 0.5 * alphaCo;
+    let reinforcementArea = m / (0.9 * gamaCo * depth * fy);
     if (alphaCo > alphaMaxCo) {
-      return res.status(400).json({ error: "Invalid section" }).end();
+      gamaCo = 1 - 0.5 * alphaMaxCo;
+      const yMax = depth * alphaMaxCo;
+      const areaMaxCo = alphaMaxCo * (1 - 0.5 * alphaMaxCo);
+      const mMax = 0.9 * 0.85 * fc * areaMaxCo * b * depth ** 2;
+      const mComp = m - mMax;
+      const fs = Math.min((630 * (yMax - 0.85 * a)) / yMax, fy);
+      const compReinforcementArea = mComp / (0.9 * fs * (depth - a));
+      reinforcementArea =
+        mMax / (0.9 * gamaCo * depth * fy) + (compReinforcementArea * fs) / fy;
     }
-
-    const gamaCo = 1 - 0.5 * alphaCo;
-
-    const reinforcementArea = m / (0.9 * gamaCo * depth * fy);
 
     const barsCount = Math.max(
       Math.ceil(reinforcementArea / (Math.PI * (d / 2) ** 2)),
@@ -49,7 +58,27 @@ export const square = async (req: Request, res: Response) => {
 
     d && (result.bars = { number: barsCount, diameter: d, area: barsArea });
 
-    return res.status(200).json({ result }).end();
+    return res
+      .status(200)
+      .json({
+        result,
+        ...(draft
+          ? {
+              draft: {
+                a,
+                depth,
+                areaCo,
+                alphaCo,
+                alphaMaxCo,
+                gamaCo,
+                reinforcementArea,
+                barsCount,
+                barsArea,
+              },
+            }
+          : {}),
+      })
+      .end();
   } catch (error) {
     console.log(error);
     return res.sendStatus(400);
